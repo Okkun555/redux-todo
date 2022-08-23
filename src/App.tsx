16a31e4a160ba5c9f02d2c2222998d417130f164
Okkun555 <y.okuno.btm@gmail.com>
2022-08-23 23:42:04 +0900

    タイトルの変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Todoリスト</h1>
+    </div>
+  );
+};
 
 export default App;
