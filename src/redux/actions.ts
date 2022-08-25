1fab85a64e1d587fe8dc8a145236917c9f2498d1
Okkun555 <y.okuno.btm@gmail.com>
2022-08-25 23:25:18 +0900

    actionの実装

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,5 +1,46 @@
-export const VISIBILITY_FILTERS = {
-  ALL: "task/all",
-  COMPLETED: "task/completed",
-  INCOMPLETE: "task/incomplete",
-} as const;
+import { ValueOf, VsibilityFilterTypes } from "./types";
+
+export type ActionTypes = {
+  ADD_TODO: "todo/add";
+  TOGGLE_TODO: "todo/toggle";
+  SET_FILTER: "filter/set";
+};
+
+type AddTodoAction = {
+  type: ValueOf<ActionTypes>;
+  payload: {
+    id: number;
+    content: string;
+  };
+};
+
+let nextIds = 0;
+
+export const addTodo = (content: string): AddTodoAction => ({
+  type: "todo/add",
+  payload: {
+    id: ++nextIds,
+    content,
+  },
+});
+
+type ToggleTodoAction = {
+  type: ValueOf<ActionTypes>;
+  payload: {
+    id: number;
+  };
+};
+
+export const toggleTodo = (id: number): ToggleTodoAction => ({
+  type: "todo/toggle",
+  payload: { id },
+});
+
+type SetFilterAction = {
+  type: ValueOf<ActionTypes>;
+  payload: {
+    filter: VsibilityFilterTypes;
+  };
+};
+
+export type TodoActions = AddTodoAction | ToggleTodoAction | SetFilterAction;
